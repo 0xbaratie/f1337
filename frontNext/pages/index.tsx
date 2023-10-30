069d414ef691e74ff937d36ceda6850c7d1c87c1
libdefi <libcreatesweb3@gmail.com>
2023-10-30 12:11:14 +0800

    Update index.tsx

--- frontNext/pages/index.tsx
+++ frontNext/pages/index.tsx
@@ -92,9 +92,9 @@ const Home: NextPage = () => {
     
     if (address &&  !connectedFarcaster) {
       // [FOR TEST]
-      const addressTest = "0x6d92e36f3c7c309f90e7ca98d06e6599649961e3"
-      SocialAccountData(addressTest, setFarcasterName, setConnectedFarcaster, setError);
-      // SocialAccountData(address, setFarcasterName, setConnectedFarcaster, setError);
+      // const addressTest = "0x6d92e36f3c7c309f90e7ca98d06e6599649961e3"
+      // SocialAccountData(addressTest, setFarcasterName, setConnectedFarcaster, setError);
+      SocialAccountData(address, setFarcasterName, setConnectedFarcaster, setError);
     }
 
     if (isSuccess && parseInt(yourNum) > 0) {
