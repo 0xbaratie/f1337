3ffc547f3afae4516a85f7509dca3e98e91f0e3d
libdefi <libcreatesweb3@gmail.com>
2024-02-04 16:51:48 +0800

    Set up metatag

--- frontNext/pages/frame/index.tsx
+++ frontNext/pages/frame/index.tsx
@@ -1,36 +1,59 @@
 import {Metadata, ResolvingMetadata} from "next";
 import RandomInterval from '../../data/RandomInterval';
 import React, { useState } from 'react';
+import { getFrameMetadata } from '@coinbase/onchainkit';
 
 type Props = {
     params: { id: string }
     searchParams: { [key: string]: string | string[] | undefined }
 }
 
-export async function generateMetadata(
-    { params, searchParams }: Props,
-    parent: ResolvingMetadata
-): Promise<Metadata> {
-    const fcMetadata: Record<string, string> = {
-        "fc:frame": "vNext",
-        "fc:frame:post_url": `${process.env['HOST']}/api/mint`,
-        "fc:frame:image": 'https://i.gyazo.com/40a269363f416f28caff4f8d9601d670.gif',
-    };
-    fcMetadata[`fc:frame:button:1`] = "Recast and click 👉 Mint";
+// export async function generateMetadata(
+//     { params, searchParams }: Props,
+//     parent: ResolvingMetadata
+// ): Promise<Metadata> {
+//     const fcMetadata: Record<string, string> = {
+//         "fc:frame": "vNext",
+//         "fc:frame:post_url": `${process.env['HOST']}/api/mint`,
+//         "fc:frame:image": 'https://i.gyazo.com/40a269363f416f28caff4f8d9601d670.gif',
+//     };
+//     fcMetadata[`fc:frame:button:1`] = "Recast and click 👉 Mint";
 
-    return {
-        title: "f1337",
-        openGraph: {
-            title: "f1337",
-            images: ['https://f1337.vercel.app/ogp.png'],
-        },
-        other: {
-            ...fcMetadata,
-        },
-        metadataBase: new URL(process.env['HOST'] || '')
-    }
-}
+//     return {
+//         title: "f1337",
+//         openGraph: {
+//             title: "f1337",
+//             images: ['https://f1337.vercel.app/ogp.png'],
+//         },
+//         other: {
+//             ...fcMetadata,
+//         },
+//         metadataBase: new URL(process.env['HOST'] || '')
+//     }
+// }
 
+const frameMetadata = getFrameMetadata({
+    buttons: [
+        {
+        label: 'Recast and click 👉 Mint',
+        },
+    ],
+    image: 'https://i.gyazo.com/40a269363f416f28caff4f8d9601d670.gif',
+    post_url: `${process.env['HOST']}/api/mint`,
+    });
+
+    export const metadata: Metadata = {
+    title: 'F1337',
+    description: 'Are you 1337?',
+    openGraph: {
+        title: 'F1337',
+        description: 'Are you 1337?',
+        images: ['https://i.gyazo.com/40a269363f416f28caff4f8d9601d670.gif'],
+    },
+    other: {
+        ...frameMetadata,
+    },
+};
 
 export default function Page() {
     const [randomNumber, setRandomNumber] = useState('1337');
