38a318adca5bb265fcb2672502fde1876e8e45b3
libdefi <libcreatesweb3@gmail.com>
2024-02-04 16:26:56 +0800

    Set test hash

--- frontNext/pages/api/getWarpcastHash.ts
+++ frontNext/pages/api/getWarpcastHash.ts
@@ -2,13 +2,13 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { NeynarAPIClient, CastParamType } from "@neynar/nodejs-sdk";
 
 
-
+// This function for development debugging
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   const client = new NeynarAPIClient(process.env.NEYNAR_API_KEY || '');
-  const url = "https://warpcast.com/dwr.eth/0x029f7cce";
+  const url = "https://warpcast.com/baratie/0x508754e9"; // Change this to your warpcast url
   const cast = await client.lookUpCastByHashOrWarpcastUrl(url, CastParamType.Url);
   console.log(cast);
   
